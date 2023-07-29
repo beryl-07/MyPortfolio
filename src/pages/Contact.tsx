6ab71984ab68f80d01bd97274c932218235064ef
Béryl HOUESSOU <berylhoe@gmail.com>
2023-07-29 12:54:03 +0100

    Make responsive Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -12,28 +12,28 @@ function Contact() {
                 </div>
                 <div className="flex w-full justify-between items-start h-[30vh] ">
                     <div className="grid w-[30%] h-full place-items-center ">
-                        <div className="text-3xl ">Bryl dev.</div>
+                        <div className="text-3xl max-lg:text-2xl">BrylDev.</div>
                     </div>
                     <div className="flex w-[50%] justify-evenly items-start text-left ">
-                        <ul className="list-none list-inside">
-                            <h3 className="font-semibold text-4xl py-3">Contact</h3>
+                        <ul className="list-none list-inside max-lg:leading-8">
+                            <h3 className="font-semibold text-4xl py-3 max-lg:text-3xl">Contact</h3>
                             <li>
-                                <span className="text-[1.4vw]">Twitter </span>
+                                <span className="text-[1.4vw] max-lg:text-sm">Twitter </span>
                             </li>
                             <li>
-                                <span className="text-[1.4vw]">Whatsapp </span>
+                                <span className="text-[1.4vw] max-lg:text-sm">Whatsapp </span>
                             </li>
                         </ul>
-                        <ul className="list-none list-inside">
-                            <h3 className="font-semibold text-4xl py-3">Other</h3>
+                        <ul className="list-none list-inside max-lg:leading-8">
+                            <h3 className="font-semibold text-4xl py-3 max-lg:text-3xl">Other</h3>
                             <li>
-                                <span className="text-[1.4vw]">LinkedIn </span>
+                                <span className="text-[1.4vw] max-lg:text-sm">LinkedIn </span>
                             </li>
                             <li>
-                                <span className="text-[1.4vw]">Github <sub>@beryl452</sub></span>
+                                <span className="text-[1.4vw] max-lg:text-sm">Github <sub>@beryl452</sub></span>
                             </li>
                             <li>
-                                <span className="text-[1.4vw]">Github <sub>@beryl-07</sub></span>
+                                <span className="text-[1.4vw] max-lg:text-sm">Github <sub>@beryl-07</sub></span>
                             </li>
                         </ul>
                     </div>
